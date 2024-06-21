b8eeeeb7386491f004b7ca311db65a22b8ae7dbb
Daniel <danielbrodowski97@gmail.com>
2024-06-21 04:43:12 +0100

    login

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -1,6 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
+  const dashboard = document.querySelector('.dashboard');
+
   const user = JSON.parse(localStorage.getItem('currentUser'));
   if (user) {
+    // const htmlTemplate = `<h1>Hello ${user.username}</h1>`;
+    // dashboard.insertAdjacentHTML('afterbegin', htmlTemplate);
     console.log(user);
   }
 });
